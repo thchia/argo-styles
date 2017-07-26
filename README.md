ce51569641ce41e63ac8b81f236a0ce726c3b527
Thomas Chia <thomas.chia@amaas.com>
2017-07-26 11:16:51 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+
+Custom semantic ui theme for a project.
+
+This project is built with create-react-app and the semantic-ui-react & storybook packages for prototyping.
---
+++ stories/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Form, Input, Message, Segment } from 'semantic-ui-react'
+
+import './styles.css'
+import '../semantic/dist/semantic.min.css'
+
+const error = true
+
+storiesOf('Button', module)
+  .add('green', () => <div>
+    <Button color='green'>Next</Button>
+    <Button basic color='green'>Next</Button>
+  </div>)
+  .add('green inverted', () => <div>
+    <Segment inverted>
+      <Button inverted color='green'>Next</Button>
+      <Button inverted basic color='green'>Next</Button>
+    </Segment>
+  </div>)
+
+storiesOf('Message', module)
+  .add('green', () => <div style={{ width: '300px' }}>
+    <Message color='green'>
+    <Message.Header>
+      Header
+    </Message.Header>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Nunc tristique odio viverra eros interdum luctus.
+      Pellentesque consectetur tortor id dictum blandit.
+    </p>
+    </Message>
+  </div>)
+  .add('green against dark', () => <Segment inverted>
+    <div style={{ width: '300px' }}>
+      <Message color='green'>
+        <Message.Header>
+          Header
+        </Message.Header>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Nunc tristique odio viverra eros interdum luctus.
+          Pellentesque consectetur tortor id dictum blandit.
+        </p>
+      </Message>
+    </div>
+</Segment>)
+
+storiesOf('Input', module)
+  .add('normal', () => <div>
+    <Input placeholder='Placeholder' />
+  </div>)
+  .add('normal error', () => <div>
+    <Input error placeholder='Placeholder' />
+  </div>)
+  .add('inverted', () => <div>
+    <Segment inverted>
+      <Input className='input-inverted' inverted placeholder='Placeholder' />
+    </Segment>
+  </div>)
+  .add('inverted error', () => <div>
+    <Segment inverted>
+      <Input className='input-inverted error' inverted placeholder='Placeholder' />
+    </Segment>
+</div>)
+
+storiesOf('Form', module)
+  .add('normal', () => <div style={{ width: '300px' }}>
+    <Form>
+      <Form.Field>
+        <input placeholder='Placeholder' />
+      </Form.Field>
+      <Form.Field>
+        <input placeholder='Placeholder' />
+      </Form.Field>
+    </Form>
+  </div>)
+  .add('normal error', () => <div style={{ width: '300px' }}>
+    <Form>
+      <Form.Field error>
+        <input placeholder='Placeholder' />
+      </Form.Field>
+      <Form.Field error>
+        <input placeholder='Placeholder' />
+      </Form.Field>
+    </Form>
+  </div>)
+  .add('inverted', () => <div style={{ width: '300px' }}>
+    <Segment inverted>
+      <Form inverted>
+        <Form.Field className='input-inverted' >
+          <input placeholder='Placeholder' />
+        </Form.Field>
+        <Form.Field className='input-inverted'>
+          <input placeholder='Placeholder' />
+        </Form.Field>
+      </Form>
+    </Segment>
+  </div>)
+  .add('inverted error', () => <div style={{ width: '300px' }}>
+    <Segment inverted>
+      <Form inverted>
+        <Form.Field className='input-inverted error' error>
+          <input placeholder='Placeholder' />
+        </Form.Field>
+        <Form.Field className='input-inverted error' error>
+          <input placeholder='Placeholder' />
+        </Form.Field>
+      </Form>
+    </Segment>
+  </div>)
